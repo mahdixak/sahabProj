1b6841b1deb856a58b3f3253d793fc7cbc165766
mahdixak <mahdiamcfc@gmail.com>
2023-12-31 00:40:50 +0330

    starting project

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+
+    static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        System.out.println("bi tarbiat");
+    }
+}
